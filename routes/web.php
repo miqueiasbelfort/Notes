51286e422da53bcf3a0778c6c25dc1a8ee154574
Miqueias_belfort <kawasousa1554@gmail.com>
2025-07-17 22:20:00 -0300

    login view page

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+
+// auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
